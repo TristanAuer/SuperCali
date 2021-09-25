885bdfa5781617a12c5625953e8a6de1258fdec5
unknown <tristanjauer@gmail.com>
2021-09-25 14:04:33 -0400

    first

---
+++ SuperCali/SuperCali/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperCali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print Supercalifragilisticexpialidocious");
+            // String of authors  
+            string word = "supercalifragilisticexpialidocious";
+            char[] charArray = word.ToCharArray();
+            foreach (char letter in charArray)
+            {
+                Console.WriteLine(letter);
+            }
+            foreach (char letter in charArray)
+            {
+                if (letter != 'i')
+                {
+                    Console.WriteLine("Not an i");
+                }
+                else
+                {
+                    Console.WriteLine(letter);
+                }
+            }
+          
+
+            Console.ReadLine();
+        }
+    }
+}
